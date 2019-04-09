3842c8368dd795b37a109bb20f0916b34194228b
Kam <kjakrzewski@student.unimelb.edu.au>
2019-04-09 12:50:41 +1000

    cleanup code

--- analyse_tweets.py
+++ analyse_tweets.py
@@ -39,19 +39,6 @@ def process_tweet(tweet, stop_words):
 
     return words
 
-
-def get_words_in_tweets(tweets):
-    all_words = []
-    for (words, sentiment) in tweets:
-        all_words.extend(words)
-    return all_words
-
-
-def get_word_features(wordlist):
-    wordlist = nltk.FreqDist(wordlist)
-    word_features = wordlist.keys()
-    return word_features
-
 def extract_features(document):
     document_words = set(document)
     features = {}
